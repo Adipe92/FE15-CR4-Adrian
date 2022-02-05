fb07f3725518c2dad5f772b0f0f9d8cad9616726
Adrian Pedziwiatr <adrianpe092@gmail.com>
2022-02-05 17:54:02 +0100

    final files created

---
+++ js/script.js
@@ -0,0 +1,60 @@
+var myArr = JSON.parse(array);
+
+console.table(myArr);
+
+for (let val of myArr) {
+
+    document.getElementById("calendar").innerHTML += ` 
+    
+    <div class="card m-2" style="width: 18rem;">
+                    <div class="m-1 d-flex justify-content-between">
+                        <div>
+                            <button type="button" id="btnclick" height="10%" class="btn btn-2 btn-info mb-1">Task</button>
+                        </div>
+                        <div>
+                            <i class="far fa-bookmark me-2"></i>
+                            <i class="fas fa-ellipsis-v"></i>
+                        </div>
+                    </div>
+                    <img src="${val.img}" class="card-img-top p-1" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${val.name}</h5>
+                        <p class="card-text text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam,
+                            unde?
+                        </p>
+                    </div>
+                    <hr class="hr">
+                    <div class="d-flex">
+                    <p class="ps-3 pt-2"><i class="fas fa-exclamation-triangle"></i> Priority level:</p>
+                    <span id="nber" class="m-2"> 0</span>
+                    </div>
+                    
+                    <p class="ps-3 pt-1"><i class="fas fa-calendar-week"></i> Deadline: ${val.date}</p>
+                    <hr class="hr">
+                    <div class="d-flex justify-content-end card-body">
+                        <div>
+                            <button type="button" class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i>
+                                Delete</button>
+                            <button type="button" class="btn btn-success btn-sm"><i class="far fa-check-circle"></i>
+                                Done</button>
+                        </div>
+
+                    </div>
+                </div> `;
+};
+
+const btns = document.querySelectorAll("#btnclick");
+const number = document.getElementById("nber");
+
+let integer = 0;
+
+console.log(btns);
+
+
+btns.forEach(btn => btn.addEventListener('click', function(){
+    
+}
+));
+
+
+
