317f319865c115f639a966d182d76291b3e574f8
Adrian Pedziwiatr <adrianpe092@gmail.com>
2022-02-07 11:01:19 +0100

    script changed

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ for (let val of myArr) {
     <div class="card m-2" style="width: 18rem;">
                     <div class="m-1 d-flex justify-content-between">
                         <div>
-                            <button type="button" id="btnclick" height="10%" class="btn btn-2 btn-info mb-1">Task</button>
+                            <button type="button" id="btnclick"  height="10%" class="btn btn-2 btn-info mb-1 btnclick">Task</button>
                         </div>
                         <div>
                             <i class="far fa-bookmark me-2"></i>
@@ -26,7 +26,7 @@ for (let val of myArr) {
                     <hr class="hr">
                     <div class="d-flex">
                     <p class="ps-3 pt-2"><i class="fas fa-exclamation-triangle"></i> Priority level:</p>
-                    <span id="nber" class="m-2"> 0</span>
+                    <span id="nber" class="nber" class="m-2"> ${val.importance}</span>
                     </div>
                     
                     <p class="ps-3 pt-1"><i class="fas fa-calendar-week"></i> Deadline: ${val.date}</p>
@@ -43,16 +43,14 @@ for (let val of myArr) {
                 </div> `;
 };
 
-const btns = document.querySelectorAll("#btnclick");
-const number = document.getElementById("nber");
-let integer = 0;
+const btns = document.querySelectorAll(".btnclick");
+const number = document.getElementsByClassName("nber");
 
-console.log(btns);
+console.log(btns); 
 
-
-btns.forEach(btn => btn.addEventListener('click', function(){
-    integer += 1;
-    number.innerHTML = integer;
+btns.forEach((btn, i) => btn.addEventListener('click', function(){
+    myArr[i].importance += 1;
+    number[i].innerHTML = myArr[i].importance; 
 }
 ));
 
